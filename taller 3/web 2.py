ca378bf5aead91538b937df4cb0e36142da59d62
HugoSabogal <88988214+Hugo-Andres-Sabogal-Perez@users.noreply.github.com>
2024-11-17 12:48:05 -0500

    cambio en script de web

--- taller 3/web 2.py
+++ taller 3/web 2.py
@@ -11,23 +11,6 @@ from selenium.webdriver.common.by import By
 url =  "https://www.metrocuadrado.com/apartamento/arriendo/chapinero"
 
 
-
-r = requests.get(url)
-
-# Verificamos que todo esté ok
-r.status_code == 200
-
-# Vamos a convertirlo en un formato más amigable para Python
-soup = BeautifulSoup(r.content)
-
-
-# Extraigamos todos los títulos principales
-# Encuentra todos los elementos <li>
-links = soup.find_all("a", href=True)
-[i['href'] for i in links]
-
-
-
 #### SELENIUM
 
 driver = webdriver.Edge()
@@ -37,15 +20,21 @@ links1 = driver.find_elements(By.XPATH, '//li[contains(@class, "sc-gPEVay")]//a'
 href1 = [link.get_attribute("href") for link in links1]  # Extrae los atributos href
 print(href1)
 
-links2 = driver.find_elements(By.XPATH, '//a[contains(@class, "sc-bdVaJa")]')  # Asegúrate de apuntar a los elementos <a>
-hrefs2 = [link.get_attribute("href") for link in links2]  # Extrae los atributos href
-print(hrefs2)
 
 
-resultado = list(set(filter(lambda x: x is not None, hrefs2)))
+resultado = list(set(filter(lambda x: x is not None, href1)))
 
 urls_filtradas = [url for url in resultado if '/inmueble' in url]
 
 ultimos_segmentos = [url.rsplit('/', 1)[-1] for url in urls_filtradas]
 
 "MC5297645" in ultimos_segmentos
+
+
+flecha = driver.find_element(By.CLASS_NAME, 'Icon-sc-1mikm6x-0 jvsOeO')
+flecha.click() 
+
+element = driver.find_element(By.XPATH, "//div[@class='Icon-sc-1mikm6x-0 jvsOeO']")
+
+wait = WebDriverWait(driver, 10)
+element = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, ".Icon-sc-1mikm6x-0.jvsOeO")))
