d0851875b9a8b3e0795814c1df313e1a83d050f8
HugoSabogal <88988214+Hugo-Andres-Sabogal-Perez@users.noreply.github.com>
2024-12-01 21:15:45 -0500

    cambios

--- taller 4/yfinance.py
+++ taller 4/yfinance.py
@@ -4,6 +4,7 @@ Created on Sun Dec  1 09:37:47 2024
 
 @author: hugos
 """
+
 import yfinance as yf
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -22,6 +23,10 @@ activos = list(Portafolio_inicial.keys())
 # Descargar datos de cierre ajustado (ajustado por splits y dividendos)
 acciones = yf.download(activos, start="2020-01-01", end="2023-12-01")
 acciones  = acciones['Close']
+
+
+
+acciones.to_excel('acciones.xlsx')
 
 benchmark = yf.download('VOO', start="2020-01-01", end="2023-12-01")
 benchmark = benchmark['Close']
@@ -45,13 +50,13 @@ ax.plot(evol_100.index,evol_100['Benchmark'], color='orange', label = 'Benchmark
 
 
 # Nombre del eje x Y tamaño  de latra 12
-plt.xlabel('Sector', fontsize=12)
+plt.xlabel('Fecha', fontsize=12)
 
 # Nombre del eje y Y tamaño  de latra 12
-plt.ylabel('Numero de Contratos', fontsize=12)
+plt.ylabel('$', fontsize=12)
 
 # Titulo del gráfico con tamaño de letra 18
-plt.title('Cantidad de contratos por sector', fontsize=18)
+plt.title('Evolución de 100$' , loc="center")
 
 #
 plt.legend()
@@ -91,7 +96,9 @@ ax.legend(wedges, labels, title="Sectores", loc="center left", bbox_to_anchor=(1
 
 
 # Mostrar el gráfico
-plt.title("Composición por sector")
+plt.title("Composición por sector", loc="center")
+plt.savefig('comp_sector.png', bbox_inches="tight")
+
 plt.show()
 
 
@@ -108,7 +115,7 @@ currency_distribution = pd.DataFrame(lista, columns=['Currency', 'Peso'])
 currency_distribution = currency_distribution.groupby(by='Currency').sum()
 
 # Crear el gráfico de pastel
-fig, ax = plt.subplots(dpi=300)
+fig, ax = plt.subplots(dpi=300, )
 
 # Crear un colormap de tonos azules
 colors = plt.cm.Blues(np.linspace(0.35, 0.8, len(currency_distribution)))
@@ -125,7 +132,8 @@ ax.legend(wedges, labels, title="Currency", loc="center left", bbox_to_anchor=(1
 
 
 # Mostrar el gráfico
-plt.title("Composición por moneda")
+plt.title("Composición por moneda", loc="center")
+plt.savefig('comp_moneda.png', bbox_inches="tight")
 plt.show()
 
 lista=[]
@@ -158,5 +166,9 @@ ax.legend(wedges, labels, title="Country", loc="center left", bbox_to_anchor=(1.
 
 
 # Mostrar el gráfico
-plt.title("Composición por País")
+plt.title("Composición por País", loc="center")
+plt.savefig('comp_pais.png',bbox_inches="tight")
 plt.show()
+
+
+
